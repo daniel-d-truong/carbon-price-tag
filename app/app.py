a35efbc3ed2fed02d1a6133aeafecfa6e2107b5e
jimzers <38634441+jimzers@users.noreply.github.com>
2020-02-01 11:48:45 -0800

    lint app.py

--- app/app.py
+++ app/app.py
@@ -3,15 +3,16 @@ from db import firebase_db
 from scrape import scrape
 from carbon_price import carbon_price
 
-
 app = Flask(__name__)
 app.register_blueprint(firebase_db, url_prefix='/db')
 app.register_blueprint(scrape, url_prefix='/scrape')
 app.register_blueprint(carbon_price, url_prefix='/get-footprint')
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
 
 if __name__ == '__main__':
     app.run()
