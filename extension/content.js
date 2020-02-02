e7fb9d40fca6ade105a238599e522d96f8df8199
Daniel Truong <ddtruong06@gmail.com>
2020-02-01 20:55:44 -0800

    Added logic to webscrape values when on the cart page

--- extension/content.js
+++ extension/content.js
@@ -1,26 +1,83 @@
+// Set functions
+const cartCarbonFootprint = () => {
+    const allCartEl = document.querySelectorAll(".sc-product-title.a-size-medium");
+    const quantityElList = document.querySelectorAll(".a-icon.a-icon-dropdown")
+    const idObj = { };
+    for (let i = 0; i < allCartEl.length; i++) {
+        // gets the ID of the amazon item
+        const item = allCartEl[i];
+        let itemUrl = item.parentElement.href;
+        let id = null;
+        if (itemUrl.includes("/gp/product/")) {
+            id = itemUrl.substring(34, 44);
+        } 
+        // gets the element of the quantity dropdown
+        const quantityEl = quantityElList[i].parentElement;
+        let quantity = quantityEl.innerText;
+        // possibly deal with some logic when the quantity is not a common one
+
+
+        // add event listener to quantityEl when the value gets changed
+        quantityEl.addEventListener("change", (event) => {
+            console.log("changed");
+        });
+        if (id) {
+            idObj[id] = quantity
+        }
+    }
+    console.log(idObj);
+}
+
+const convertToPounds = (ounces) => {
+    return ounces*(0.0625);
+}
+
+const grabWeightPerQuan = (term) => {
+    let i = upperProductName.indexOf(term) - 2;
+    let amount = "";
+
+    while (upperProductName.charAt(i) !== ' ') {
+        amount = upperProductName[i] + amount;
+        i--;
+    }
+    weight = convertToPounds(Number(amount));
+}
+
+const url = window.location.href;
+// logic for doing the cart
+if (url.includes("cart")) {
+    cartCarbonFootprint();
+} 
+
 // Gets the Item Weight, Shipping Weight, and asin
 let a = document.querySelector("#detail-bullets");
 let feature_list = a.getElementsByTagName("table")[0].getElementsByTagName('tbody')[0].getElementsByTagName('tr')[0].getElementsByTagName('td')[0].getElementsByTagName('div')[0].getElementsByTagName('ul')[0].getElementsByTagName('li')
 let item_weight = 0;
-let ship_weight = 0;
+let ship_weight = 0.5;
 let asin = null;
+let weightFound = false;
 for (const feature of feature_list) {
     let feature_arr = feature.innerText.split(': ');
+    // Not guaranteed to exist
     if (feature_arr[0] === "Item Weight") {
         item_weight = Number(feature_arr[1]);
+        weightFound = true;
     }
     if (feature_arr[0] === "Shipping Weight") {
         ship_weight = Number(feature_arr[1]);
+        weightFound = true;
     }
+    // Guaranteed to always exist
     if (feature_arr[0] === "ASIN") {
         asin = feature_arr[1]
     }
 }
+let weight = item_weight + ship_weight;
 console.log([item_weight, ship_weight, asin]);
 
 // Gets the name of the product
 const productTitleEl = document.querySelector("#productTitle");
-const productName = productTitleEl.innerHTML.split(" ")[0];
+const productName = productTitleEl.innerText.split(" ")[0];
 
 // Gets the ingredients of the product
 const importantInfoEl = document.querySelector("#important-information").querySelectorAll(".content");
@@ -29,25 +86,27 @@ for (const infoEl of importantInfoEl){
     if (infoEl.getElementsByTagName("span")[0].innerText.toUpperCase() == "INGREDIENTS") {
         let ingredientsString = infoEl.innerText.replace("Ingredients\n", "");
         ingredientsString = ingredientsString.split("*").join("").split("\n").join("");
-        ingredients = ingredientsString.split(", ");
+        ingredients = ingredientsString.split(", ").join(" ").split(" ");
         console.log(ingredients);
     }
 }
 
+const upperProductName = productTitleEl.innerText.toUpperCase();
+// Gets the weight of the item per one quantity when weight can't be found
+if (!weightFound) {
+    console.log(upperProductName);
+    if (upperProductName.includes("OUNCE")) {
+        grabWeightPerQuan("OUNCE");
+    } else if (upperProductName.includes("OZ")) {
+        grabWeightPerQuan("OZ");
+    }
+}
+
+console.log("Weight: " + weight);
+
 // Makes a POST request which would send information to the backend and retreives the carbon pricing in response. This should be redone 
 // whenever the user updates their address.
-await fetch("http://127.0.0.1:5000/item", {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        'name': productName,
-        'ingredients': ingredients,
-        'weight': item_weight + ship_weight,
-        'carbon_location': ''
-    })
-});
+
 
 // Adds the new element into the Amazon page
 const el = document.querySelector("#priceblock_ourprice_row");
@@ -69,3 +128,4 @@ carbonData2.innerHTML = "est. $1.23 (equivalent to 60 kg of CO2)";
 carbonRow.appendChild(carbonData1);
 carbonRow.appendChild(carbonData2);
 el.insertAdjacentElement('afterend', carbonRow);
+
