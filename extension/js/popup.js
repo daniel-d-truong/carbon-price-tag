b530ed9be987a52aa3d1ba067e7697e79e2113d6
whykatherine <whykatherine@gmail.com>
2020-02-01 20:04:20 -0800

    Dynamic gauge arc for carbon spending

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,6 +1,19 @@
 // Note that it's very hard to debug this since it is a popup. 
 document.addEventListener("DOMContentLoaded", () => {
 
+    const carbonSpending = parseFloat(document.querySelector("#carbonSpending").innerHTML);
+    const budget = parseFloat(document.querySelector("#budget").value);
+    const pct = carbonSpending / budget;
+    const arc = document.querySelector("#arc");
+    const offsetMax = 1000;
+    const offsetMin = 580;
+    arc.style["stroke-dashoffset"] = offsetMax - pct * (offsetMax - offsetMin);
+    if (carbonSpending <= budget) {
+        arc.style.stroke = "#25922F";
+    } else {
+        arc.style.stroke = "#C73E41";
+    }
+
     // username
     const username_form = document.querySelector("#username-form");
     username_form.addEventListener("submit", (event) => {
